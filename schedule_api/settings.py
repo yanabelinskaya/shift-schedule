f9c1c64340e7e671a241b3780d5cf3b18c78ee6f
yanabelinskaya <144311460+yanabelinskaya@users.noreply.github.com>
2026-05-06 11:33:26 +0300

    Configure static files for Render

--- schedule_api/settings.py
+++ schedule_api/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -280,6 +281,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 BACKUP_STORAGE_DIR = Path(os.getenv('BACKUP_STORAGE_DIR', str(BASE_DIR / 'backups'))).expanduser()
