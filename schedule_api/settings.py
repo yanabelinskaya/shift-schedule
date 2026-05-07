e2f6ea53ec67a9548a5d0ce99f8c526999d26af0
yanabelinskaya <144311460+yanabelinskaya@users.noreply.github.com>
2026-05-07 21:31:13 +0300

    Configure email timeout

--- schedule_api/settings.py
+++ schedule_api/settings.py
@@ -350,6 +350,7 @@ EMAIL_USE_TLS = _get_bool('EMAIL_USE_TLS', True)
 EMAIL_USE_SSL = _get_bool('EMAIL_USE_SSL', False)
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '10'))
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'no-reply@example.com')
 
 LOGGING = {
